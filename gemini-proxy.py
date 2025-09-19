696dff7f9ee80317f085ccb5d1a3683da46d9ec3
Oleg Semenov <oleg@belvg.com>
2025-09-19 11:11:59 +0200

    Add BelVG copyright license header

--- gemini-proxy.py
+++ gemini-proxy.py
@@ -1,3 +1,20 @@
+"""
+ BelVG LLC.
+
+ NOTICE OF LICENSE
+
+ This source file is subject to the EULA
+ that is bundled with this package in the file LICENSE.txt.
+ It is also available through the world-wide-web at this URL:
+ https://store.belvg.com/BelVG-LICENSE-COMMUNITY.txt
+
+ *******************************************************************
+ @category   BelVG
+ @author     Oleg Semenov
+ @copyright  Copyright (c) BelVG LLC. (http://www.belvg.com)
+ @license    http://store.belvg.com/BelVG-LICENSE-COMMUNITY.txt
+
+"""
 import os
 import requests
 from flask import Flask, request, jsonify, Response
